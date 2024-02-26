96a29e4d877a69fa165802058175baa4bcfb3138
allankama1 <allankamau0254@gmail.com>
2024-02-26 05:54:24 -0500

    first commit

---
+++ Hosiapp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from Hosiapp.models import Users,Product
+
+# Register your models here.
+admin.site.register(Users)
+admin.site.register(Product)
---
+++ Hosiapp/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.urls import path
+from Hosiapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index,name='index'),
+    path('innerpage/', views.inner,name='inner'),
+    path('register/', views.register,name='register'),
+    path('login/', views.login,name='login'),
+ ]
